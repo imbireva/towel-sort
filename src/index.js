0e2485265debe18c401945a520d90f9e83a4c95c
Daria Imbireva <imbireva.daria@gmail.com>
2022-03-09 21:07:47 +0300

    update solution

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  let resultArr = [];
+  if (matrix !== undefined) {
+    let matrixCopy = matrix.slice(); // create matrix copy because reverse() change original array
 
-    let result = [];
+    matrixCopy.forEach((element, index) => {
+      if (index % 2 !== 0) { // use reverse() if index of subarray is odd number
+        element.reverse(); // reverse() change elements order in array [1, 2, 3] -> [3, 2, 1]
+      }
+      element.forEach((num) => { // work with subarray elements
+        resultArr.push(num);
+      })
+    })
 
-    if (matrix !== undefined) {
-        matrix.forEach((element, index) => {
-            if (index % 2 !== 0) {
-                element.reverse()
-            }
-    
-            element.forEach((num) => {
-                result.push(num)
-            })
-        })
-        return result;
-    }
-
-    else {
-        return [];
-    }
-      
+  }   
+  return resultArr;  
 }
+
